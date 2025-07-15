bf1e93e2c1b62674dd8eb670521aa6db1e4142bc
ToniTaniTuta <65725126+ToniTaniTuta@users.noreply.github.com>
2025-07-15 22:11:47 +0200

    image update

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,13 +1,12 @@
-import mock01 from '../assets/images/mock01.png';
-import mock02 from '../assets/images/mock02.png';
-import mock03 from '../assets/images/mock03.png';
-import mock04 from '../assets/images/mock04.png';
-import mock05 from '../assets/images/mock05.png';
-import mock06 from '../assets/images/mock06.png';
-import mock07 from '../assets/images/mock07.png';
-import mock08 from '../assets/images/mock08.png';
-import mock09 from '../assets/images/mock09.png';
-import mock10 from '../assets/images/mock10.png';
+import mock01 from '../assets/images/gozochannel.png';
+import mock02 from '../assets/images/bookora.jpg';
+import mock03 from '../assets/images/montekristo.png';
+import mock04 from '../assets/images/orderista.png';
+import mock06 from '../assets/images/wakame.jpg';
+import mock07 from '../assets/images/bern.jpeg';
+import mock08 from '../assets/images/billynx.png';
+import mock09 from '../assets/images/montekristo.png';
+
 import '../assets/styles/Project.scss';
 
 function Project() {
@@ -40,7 +39,7 @@ function Project() {
                 </div>
 
                 <div className="project">
-                    <img src={mock05} className="zoom" alt="Bookora Table Booking" width="100%" />
+                    <img src={mock02} className="zoom" alt="Bookora Table Booking" width="100%" />
                     <h2>Bookora Table – Restaurant Booking</h2>
                     <p>A tailored restaurant reservation system with smart slot management and real-time availability.</p>
                 </div>
